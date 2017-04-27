e8a8af2d426c25a91adf05304299db9ca08f0c27
Edward Apostol <edward.apostol.github@gmail.com>
2017-04-27 00:30:43 -0400

    add watch to gulp file.babel.js to test monitor changes to scss
    added watch method and tested to see if .scss files were converted to
    .css files on update.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,13 +19,27 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 
 
+
 let sassFn = () =>{
-    return gulp.src('app/scss/main.scss')
+    console.log('sassifying scss');
+    return gulp.src('./app/scss/main.scss')
         .pipe(sass()) // Using gulp-sass
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest('./app/css'))
 };
 
+/* add a watch to monitor changes to files
+* gulp.watch('files-to-watch', ['tasks', 'to', 'run']);
+* or
+* gulp.task('watch',function(){});
+* or ES6
+* gulp.task('watch', ()=>{} );
+*/
+// gulp.watch('app/scss/**/*.scss', ['sassFn'])
 // sassify code
-gulp.task('sass',sassFn );
+// gulp.task('sass',sassFn );
+let watchFn = ()=>{
+    gulp.watch('app/scss/**/*.scss', [sassFn]);
+};
+gulp.task('watch',watchFn);
 
 // gulp.task('default', () => console.log('Default task called'));
